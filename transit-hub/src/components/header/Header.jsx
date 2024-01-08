c75b7f50ffee2b3d6116c54269b83a01c2ce9bd5
AnnaZiankevich <anna.ziankevich@gmail.com>
2024-01-09 02:02:33 +0300

    Responsive drop-down menu

--- transit-hub/src/components/header/Header.jsx
+++ transit-hub/src/components/header/Header.jsx
@@ -1,9 +1,58 @@
-import React from "react"
+import React, { useState } from "react"
+import { Link } from 'react-router-dom'
+import './header.scss'
+import burgerMenu from '../../assets/img/burger-menu.svg'
+import logo from '../../assets/img/logo.svg'
 
 function Header() {
+  const [visible, setVisible] = useState(false);
+
+  const clickHandler = () => {
+    setVisible((prev) => !prev);
+  }
+
   return (
     <div className="header">
-        Header
+      <div className="header__menu">
+        <div className="header__content">
+            <div className="header__burger-menu" onClick={() => clickHandler()}>
+                <div className="header__burger-menu-icon">
+                  <img src={burgerMenu} alt="burger-menu" />
+                </div>
+                <div className="header__burger-menu-text">
+                  <span>Меню</span>
+                </div>
+            </div>
+            <div className="header__logo">
+                <img src={logo} alt="logo" />
+            </div>
+            <div className="header__phone">
+              <a href="tel:+7 997 443 73 83">+7 997 443 73 83</a>
+            </div>
+        </div>
+        <div className={visible ? "header__hidden-menu" : "header__hidden-menu hidden"}>
+            <ul className="header__menu-list">
+                <Link to="/">
+                  <li className="header__menu-item">Главная</li>
+                </Link>
+                <Link to="/about-us">
+                  <li className="header__menu-item">О нас</li>
+                </Link>
+                <Link to="/car">
+                  <li className="header__menu-item">Легковой автомобиль</li>
+                </Link>
+                <Link to="/freight-car">
+                  <li className="header__menu-item">Грузовой автомобиль</li>
+                </Link>
+                <Link to="/career">
+                  <li className="header__menu-item">Карьера</li>
+                </Link>
+                <Link to="/contacts">
+                  <li className="header__menu-item">Контакты</li>
+                </Link>
+            </ul>
+        </div>
+      </div>
     </div>
   );
 }
