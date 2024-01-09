616549fedd983e967512604b468f50e154eb95f5
AnnaZiankevich <anna.ziankevich@gmail.com>
2024-01-09 22:09:11 +0300

    Add responsive footer

--- transit-hub/src/components/footer/Footer.jsx
+++ transit-hub/src/components/footer/Footer.jsx
@@ -1,10 +1,30 @@
 import React from "react"
+import './footer.scss'
+import darkLogo from '../../assets/img/dark-logo.svg'
 
 function Footer() {
   return (
-    <div className="footer">
-      Footer
-    </div>
+    <footer className="footer">
+      <div className="footer__content">
+        <div className="footer__info">
+          <span>
+            ИП Делирович Я.Н. <br />
+            Все права защищены, 2022
+          </span>
+        </div>
+        <div className="footer__logo">
+          <img src={darkLogo} alt="dark-logo" />
+        </div>
+        <div className="footer__contacs">
+            <div className="footer__contacs-text">
+              Мы на связи с вами!
+            </div>
+            <div className="footer__contacts-phone">
+              <a href="tel:+7 997 443 73 83">+7 997 443 73 83</a>
+            </div>
+        </div>
+      </div>
+    </footer>
   );
 }
 
