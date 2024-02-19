cb8d987e8e41d851b1cafec903a77a06f1bc3963
Anna <anna.ziankevich@gmail.com>
2024-02-19 17:57:42 +0300

    Start page base

--- transit-hub/src/views/start-page/StartPage.jsx
+++ transit-hub/src/views/start-page/StartPage.jsx
@@ -3,7 +3,12 @@ import React from "react"
 function StartPage() {
   return (
     <div className="start-page">
-      StartPage
+      <div className="start-page__content">
+          <div className="start-page__main-info">
+              <div className="start-page__text"></div>
+              <div className="start-page__img"></div>
+          </div>
+      </div>
     </div>
   );
 }
