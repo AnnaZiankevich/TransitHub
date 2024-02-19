e79bd4a2e1e3f4fc7572f62f72cdb9cf8bb0aca8
AnnaZiankevich <anna.ziankevich@gmail.com>
2024-02-20 01:10:45 +0300

    Start block

--- transit-hub/src/views/start-page/StartPage.jsx
+++ transit-hub/src/views/start-page/StartPage.jsx
@@ -1,12 +1,38 @@
 import React from "react"
+import './start-page.scss'
+import img1 from '../../assets/img/img1.png'
+import img2 from '../../assets/img/img2.png'
+import sideText from '../../assets/img/side-text.png'
 
 function StartPage() {
   return (
     <div className="start-page">
       <div className="start-page__content">
           <div className="start-page__main-info">
-              <div className="start-page__text"></div>
-              <div className="start-page__img"></div>
+            <div className="start-page__text">
+              <div className="start-page__title">
+                TransitHub: ваше удобство, наша доставка
+              </div>
+              <div className="start-page__sub-title">
+                От двери до двери или до пункта выдачи
+              </div>
+            </div>
+            <div className="start-page__images">
+              <div className="start-page__img1">
+                <img src={img1} alt="img1" />
+              </div>
+              <div className="start-page__img2">
+                <img src={img2} alt="img2"/>
+              </div>
+              <div className="start-page__button">
+                <a type="button" href="#">
+                  Узнать больше
+                </a>
+              </div>
+              <div className="start-page__side-text">
+                <img src={sideText} alt="side-text" />
+              </div>
+            </div>
           </div>
       </div>
     </div>
